2a7ade55197b22b096892dbf15baecccc067b734
Sixels <sixels@protonmail.com>
2024-09-29 16:54:14 -0300

    Update login policies

--- Auth/Policies/LoginPolicyOption.cs
+++ Auth/Policies/LoginPolicyOption.cs
@@ -1,25 +1,29 @@
 using ManeKani.Auth.Ory;
 using ManeKani.DB;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ManeKani.Auth.Policies;
 
 public static class LoginPolicy
 {
-    public const string LoginComplete = "login_complete";
-    public const string LoginIncomplete = "login_incomplete";
+    public const string DenyIncomplete = "deny_incomplete";
+    public const string AllowIncomplete = "allow_incomplete";
 
     public static void AddLoginPolicy(this AuthorizationOptions options)
         => options.AddLoginPolicy(OryAuthenticationDefaults.AuthenticationScheme);
 
     public static void AddLoginPolicy(this AuthorizationOptions options, string scheme)
     {
-        options.AddPolicy(LoginIncomplete, policy => policy.BaseLoginPolicy(scheme));
-
-        options.AddPolicy(LoginComplete, policy => policy
-            .BaseLoginPolicy(scheme)
-            .Requirements.Add(new CompletedUserRequirement())
+        options.AddPolicy(DenyIncomplete, policy =>
+            policy
+                .BaseLoginPolicy(scheme)
+                .Requirements.Add(new CompletedUserRequirement())
         );
+        options.AddPolicy(AllowIncomplete, policy => policy.BaseLoginPolicy(scheme));
+
     }
 
     private static AuthorizationPolicyBuilder BaseLoginPolicy(this AuthorizationPolicyBuilder policy, string scheme)
@@ -30,7 +34,7 @@ public static class LoginPolicy
 public class CompletedUserRequirement : IAuthorizationRequirement
 { }
 
-public class CompletedUserHandler(ManeKaniDatabase db) : AuthorizationHandler<CompletedUserRequirement>
+public class CompletedUserRequirementHandler(ManeKaniDatabase db) : AuthorizationHandler<CompletedUserRequirement>
 {
     readonly ManeKaniDatabase _db = db;
 
@@ -39,6 +43,14 @@ public class CompletedUserHandler(ManeKaniDatabase db) : AuthorizationHandler<Co
         var user = new OryUser(context.User);
         if (await _db.IsUserComplete(user.Id))
         {
+            context.Succeed(requirement);
+        }
+        else
+        {
+            if (context.Resource is HttpContext httpContext)
+            {
+                httpContext.Response.Redirect("/settings/account");
+            }
             context.Succeed(requirement);
         }
     }
